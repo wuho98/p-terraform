d397072a74bdc65f425da7f253c27ef8b9e4bb35
wuho98 <dngh112@gmail.com>
2026-05-26 10:44:15 +0900

    first commit

---
+++ src/main/kotlin/com/back/HomeController.kt
@@ -0,0 +1,22 @@
+package com.back
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import software.amazon.awssdk.services.s3.S3Client
+
+@RestController
+class HomeController(
+    private val s3Client: S3Client
+) {
+
+    @GetMapping
+    fun main(): String {
+        return "Hi"
+    }
+
+    @GetMapping("/buckets")
+    fun buckets(): List<String> {
+        return s3Client.listBuckets().buckets().map { it.name() }
+    }
+
+}
